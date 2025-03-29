ecd1a60c626d3eb8b16bfa30faf55c277b564641
kay9999999 <shivam.samant.ss@gmail.com>
2025-03-29 19:59:05 +0530

    latest changes

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,25 @@
 module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::security",
+  {
+    name: "strapi::cors",
+    config: {
+      origin: ["http://localhost:3000"], // Allow requests from your Next.js frontend
+      methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"],
+      headers: [
+        "Content-Type",
+        "Authorization",
+        "Origin",
+        "Accept",
+        "X-Requested-With",
+      ],
+    },
+  },
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,11 @@
-module.exports = () => ({});
+module.exports = ({ env }) => ({
+  upload: {
+    config: {
+      provider: "local",
+      sizeLimit: 100000000,
+      providerOptions: {
+        // other provider options
+      },
+    },
+  },
+});
